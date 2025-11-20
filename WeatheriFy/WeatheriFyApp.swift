72863ba963e55d3d8eddce41c38e7bccac9064ff
ArnabDh <arnabdhara2022@gmail.com>
2025-11-20 14:47:58 +0530

    Initial Commit

---
+++ WeatheriFy/WeatheriFyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatheriFyApp.swift
+//  WeatheriFy
+//
+//  Created by Developer on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatheriFyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
